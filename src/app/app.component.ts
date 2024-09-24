1d7d046c680b91b36f453c450328cc535793e41f
saraitc19000 <maria.tuncab@cbtis72.edu.mx>
2024-09-24 10:52:55 -0500

    Primer commit en Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bases';
+  public title: string = 'Vivan Los Minions';
+  public counter: number = 10;
+
+  increaseBy (value:number) :void {
+    this.counter += value;
+  }
+
+  resert() {
+    this.counter = 10;
+  }
 }
